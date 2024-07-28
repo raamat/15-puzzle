1b00605be902f94c01e5e5d23cb7c51f46ac4dba
Raamat <raamat@bk.ru>
2024-07-28 23:16:17 +0300

    feat: change position by click

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Игра «Пятнашки» на основе видеоурока Елены Литвиновой
 
 * Ссылка на урок: https://youtu.be/U4E8ubLnGvI
@@ -8,7 +7,11 @@
 - [Pug](https://pugjs.org/) — шаблонизатор и HTML-препроцессор
 - [Stylus](https://stylus-lang.com/) — CSS-препроцессор
 - [Gulp](https://gulpjs.com/) — менеджер задач
-- [dataset](https://doka.guide/js/element-dataset/) — cвойство dataset позволяет считывать или устанавливать любые дата-атрибуты на HTML-элементе
+- [dataset](https://doka.guide/js/element-dataset/) — свойство dataset позволяет считывать или устанавливать любые дата-атрибуты на HTML-элементе
+- Методы массива: [map()](https://doka.guide/js/array-map/), [sort()](https://doka.guide/js/array-sort/), [flat()](https://doka.guide/js/array-flat/)
+- [Колбэки](https://learn.javascript.ru/callbacks)
+- [Делегирование событий](https://learn.javascript.ru/event-delegation)
+- Метод [closest()](https://doka.guide/js/element-closest/) — ищет и возвращает ближайший (начиная с самого элемента) родительский элемент, соответствующий указанному CSS-селектору. 
 
 ## Установка и запуск проекта
 
--- src/js/index.js
+++ src/js/index.js
@@ -20,6 +20,25 @@ document.querySelector("#shuffle").addEventListener("click", () => {
   setPositionItems(matrix);
 });
 
+/** 3. Change position by click */
+const blankNumber = 16;
+containerNode.addEventListener("click", (event) => {
+  const buttonNode = event.target.closest("button");
+  if (!buttonNode) {
+    return;
+  }
+
+  const buttonNumber = Number(buttonNode.dataset.matrixId);
+  const buttonCoords = findCoordinatesByNumber(buttonNumber, matrix);
+  const blankCoords = findCoordinatesByNumber(blankNumber, matrix);
+  const isValid = isValidForSwap(buttonCoords, blankCoords);
+
+  if (isValid) {
+    swap(blankCoords, buttonCoords, matrix);
+    setPositionItems(matrix);
+  }
+});
+
 function getMatrix(arr) {
   const matrix = [[], [], [], []];
   let y = 0;
@@ -57,4 +76,31 @@ function shuffleArray(arr) {
     .map((value) => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
     .map(({ value }) => value);
+}
+
+function findCoordinatesByNumber(number, matrix) {
+  for (let y = 0; y < matrix.length; y++) {
+    for (let x = 0; x < matrix[y].length; x++) {
+      if (matrix[y][x] === number) {
+        return { x, y };
+      }
+    }
+  }
+  return null;
+}
+
+function isValidForSwap(coords1, coords2) {
+  const diffX = Math.abs(coords1.x - coords2.x);
+  const diffY = Math.abs(coords1.y - coords2.y);
+
+  return (
+    (diffX === 1 || diffY === 1) &&
+    (coords1.x === coords2.x || coords1.y === coords2.y)
+  );
+}
+
+function swap(coords1, coords2, matrix) {
+  const coords1Number = matrix[coords1.y][coords1.x];
+  matrix[coords1.y][coords1.x] = matrix[coords2.y][coords2.x];
+  matrix[coords2.y][coords2.x] = coords1Number;
 }
